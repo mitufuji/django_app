7cc7b0621f23538cfb26df272f28933dee75be3a
mithufuji <tess0141@yahoo.co.jp>
2024-02-03 07:34:29 +0900

    リポジトリ作成

---
+++ hello/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Friend
+from .forms import HelloForm
+
+def index(request):
+    data = Friend.objects.all()
+    params = {
+        'title': 'Hello',
+        'data': [],
+    }
+    return render(request, 'hello/index.html', params)
+
+#create model
+def create(request):
+    params = {
+        'title':'Hello',
+        'form': HelloForm(),
+    }
+    if (request.method == 'POST'):
+        name = request.POST['name']
+        mail = request.POST['mail']
+        gender = 'gender' in request.POST
+        age = int(request.POST['age'])
+        birth = request.POST['birthday']
+
+        friend = Friend(name=name,mail=mail,gender=gender,age=age,birthday=birth,)
+        friend.save()
+        return redirect(to='/hello')
+    return render(request, 'hello/create.html', params)
+
